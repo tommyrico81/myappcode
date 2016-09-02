40ed7f04d25c0c1bc98ab59f52c40f6edaff317b
Tombo <tommyrico81@gmail.com>
2016-09-01 20:31:47 -0700

    update

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-Hello, This is a test
+<html>
+<head>
+<title>
+Test App +
+</title>
+</head>
+
+<body>
+
+
+<p>Hello Test App</p>
+<div id="main">>
+<div id="box1">
+
+
+
+
+
+
+
+
+
+
+</div>//endbox1
+</div>//endmain
+</body>
+</html>
