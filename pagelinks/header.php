8e3da59ab2852e494560fd94c43091566fe22636
tommyrico81 <tommyrico81@gmail.com>
2017-06-01 00:13:39 +0000

    update_v1

--- pagelinks/header.php
+++ pagelinks/header.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>
-<img src="images/app_icon_test.png" alt="app_test_icon" style="width:5px,height:5px;">
+<link rel="testappicon" href="images/app_icon_test.ico"/>
 </title>
 <link rel="stylesheet" type="text/css" href="styles/layout.css">
 </head>
