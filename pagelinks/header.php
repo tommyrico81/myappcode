d8a45d528830a1f07a326550f90582c01548fd5f
tommyrico81 <tommyrico81@gmail.com>
2017-06-01 00:28:27 +0000

    update

--- pagelinks/header.php
+++ pagelinks/header.php
@@ -2,6 +2,6 @@
 <head>
 <title>
 </title>
-<link rel="testappicon" href="images/app_icon_test.ico"/>
+<link rel="shortcut icon" href="images/app_icon_test.ico"/>
 <link rel="stylesheet" type="text/css" href="styles/layout.css">
 </head>
