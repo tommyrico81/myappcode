c00183306b2084140e94ec66e9c9e9be49efe19b
tommyrico81 <tommyrico81@gmail.com>
2017-06-01 02:39:23 +0000

    update

--- phpincludes/header.php
+++ phpincludes/header.php
@@ -3,7 +3,9 @@
 <title>
 TestPage
 </title>
-
+<!--css-->
+<link rel="stylesheet" type="text/css" href="styles/themes/layout.css">
+<!--End-->
 <link rel="shortcut icon" href="images/app_icon_test.ico"/>
 <!--JQueryMobile-->
 <link rel="stylesheet" href="styles/themes/mobile_themes.min.css" />
@@ -11,6 +13,6 @@ TestPage
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
 <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-<!--//End-->
+<!--End-->
 
 </head>
