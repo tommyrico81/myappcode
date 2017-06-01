c3b6e240fdb6ea1cd865fe78ad46f4c7afb926ed
tommyrico81 <tommyrico81@gmail.com>
2017-06-01 02:11:06 +0000

    update

--- pagelinks/header.php
+++ pagelinks/header.php
@@ -1,7 +1,16 @@
 <html>
 <head>
 <title>
+TestPage
 </title>
+
 <link rel="shortcut icon" href="images/app_icon_test.ico"/>
-<link rel="stylesheet" type="text/css" href="styles/layout.css">
+//JQueryMobile
+<link rel="stylesheet" href="themes/mobile_themes.min.css" />
+<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+//End
+
 </head>
